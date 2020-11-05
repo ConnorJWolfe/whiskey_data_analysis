db71328a6ace50875a1874eb12c24b0d8794cdde
Connor Wolfe <56277155+ConnorJWolfe@users.noreply.github.com>
2020-11-04 21:39:51 -0500

    Add files via upload

---
+++ distilleries_data.R
@@ -0,0 +1,14 @@
+# load packages
+pacman::p_load(tidyverse)
+
+# read csv file
+read_csv('whiskey_distilleries.csv') -> whiskey_distilleries
+
+# select only needed columns
+whiskey_distilleries[c(1,2,3,4,5)] -> cleaned_distilleries
+
+# set columns names
+colnames(cleaned_distilleries) <- (c('Distillery','County','Whiskies', 'Votes','Rating')) 
+
+# write df to csv fiel for analysis
+write.csv(cleaned_distilleries,"distilleries_with_rating.csv", row.names = TRUE)
