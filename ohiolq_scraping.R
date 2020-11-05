a139075b74bd0e49bad120f9051d2a60bcb42b91
Connor Wolfe <56277155+ConnorJWolfe@users.noreply.github.com>
2020-11-04 21:50:46 -0500

    Add files via upload

---
+++ ohiolq_scraping.R
@@ -0,0 +1,17 @@
+# load packages
+pacman::p_load(tidyverse, dplyr, tidyr) 
+
+# read csv file from Chrom DataMiner
+ohiolq = read_csv('dataminer.csv')
+
+# select only needed columns from original csv file write to new df
+ohiolq[c(2,3,4)] -> bourbon
+
+# remove NA values from df 
+bourbon %>% drop_na() -> bourbon_clean
+
+# set column names
+colnames(bourbon_clean) <- (c('Product Name','Category','Price'))
+
+# write df to csv fiel for analysis
+write.csv(bourbon_clean,"ohiolq_dataset.csv", row.names = TRUE)
